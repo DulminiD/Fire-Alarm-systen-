b2875f52fa6ba6c4d7e83c391e38412af0f15f24
RaveenD <51952105+RaveenD@users.noreply.github.com>
2020-04-30 01:29:32 +0530

    route('/MailSender')

--- Sensor/REST_API/routes/Floor.js
+++ Sensor/REST_API/routes/Floor.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 let Floor = require('../model/Floor.model');
 const axios = require('axios');
+const nodemailer = require("nodemailer");
 
 
 router.route('/all').get((req, res) => {
@@ -79,5 +80,60 @@ router.route('/MaxRoomCount').get((req, res) => {
         })
         .catch(err => res.status(400).json('Error: ' + err));
 });
+
+router.route('/MailSender').get(async (req,res)=>{
+    try {
+        const res = await axios.get('http://localhost:5000/all');
+        let floors = res.data;
+        let listOfRooms = ``;
+
+        floors.map((floor, i) => {
+            listOfRooms += `Floor No:  ${i + 1}`;
+            floor.Rooms.map((room, i) => {
+                if (room.Active) {
+                    if (room.SmokeLevel > 5 || room.CO2Level > 5)
+                        listOfRooms += `\nroom No: ${i + 1} `
+                    if (room.SmokeLevel > 5) {
+                        listOfRooms += `#Smoke Level- warning\t`
+                    }
+                    if (room.CO2Level > 5) {
+                        listOfRooms += `#CO2 Level- warning `
+                    }
+                }
+            })
+            listOfRooms += "\n-------------------------------------\n"
+        })
+        MailSender(listOfRooms);
+    } catch (err) {
+        console.error(err);
+    }
+    res.sendStatus(200);
+});
+
+function MailSender(text) {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'hanger24x7@gmail.com',
+            pass: '1qaz2wsx@'
+        }
+    });
+
+    var mailOptions = {
+        from: 'hanger24x7@gmail.com',
+        //change email address to test it
+        to: 'hanger24x7@gmail.com',
+        subject: 'Details From Sensors',
+        text: text
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
 
 module.exports = router;
